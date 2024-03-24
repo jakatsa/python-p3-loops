8ab36f316e5e215bbaaac8b27a8ea58e162e0d79
Joe <jakatsa5@gmail.com>
2024-03-25 02:58:55 +0300

    completed

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ function happyNewYear() {
   Write a method `fizzbuzz_printer` that prints the numbers from 1 to 100. For
   multiples of three, print "Fizz" instead of the number and for the multiples
   of five print "Buzz". For numbers which are multiples of both three and five,
-  print "FizzBuzz".
+  print "FizzBuzzs".
 */
 function fizzbuzzPrinter() {
   for (let num = 1; num <= 100; num++) {
--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+
+# Call the function
+happy_new_year()
+# Print "Happy New Year!" on a new line
+print('Happy New Year!\n')
+
+        
+  
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
-def fizzbuzz():
-    # code goes here!
-    pass
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+# Call the function
+fizzbuzz_printer()
+
+
